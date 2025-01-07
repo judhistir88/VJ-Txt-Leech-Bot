b9a7f95d79338f89e2ef76c110513b1ad109da85
Judhistir <70930235+judhistir88@users.noreply.github.com>
2025-01-07 22:59:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ bot = Client(
 
 @bot.on_message(filters.command(["start"]))
 async def start(bot: Client, m: Message):
-    await m.reply_text(f"<b>Hello {m.from_user.mention} 👋\n\n I Am A Bot For Download Links From Your **.TXT** File And Then Upload That File On Telegram So Basically If You Want To Use Me First Send Me /upload Command And Then Follow Few Steps..\nFor Thumb Url use direct thumb link from telegraph.ph or any direct download link website\n\nUse /stop to stop any ongoing task.</b>")
+    await m.reply_text(f"<b>Hello {m.from_user.mention} 👋\n\n I Am A Bot For Download Links From Your **.TXT** File And Then Upload That File On Telegram So Basically If You Want To Use Me First Send Me /upload Command And Then Follow Few Steps..\nFor Thumb Url use direct thumb link from telegraph.ph or any direct download link website\nDirect Download Site Example - https://postimages.org/\n\nUse /stop to stop any ongoing task.</b>")
 
 
 @bot.on_message(filters.command("stop"))
