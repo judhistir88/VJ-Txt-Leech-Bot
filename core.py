22ef388bf22ffb01300bb9c7f5a1640b91e615dd
Judhistir <70930235+judhistir88@users.noreply.github.com>
2025-01-07 22:20:27 +0530

    Update core.py

--- core.py
+++ core.py
@@ -183,7 +183,7 @@ async def download_video(url,cmd, name):
 
 
 async def send_doc(bot: Client, m: Message,cc,ka,cc1,prog,count,name):
-    reply = await m.reply_text(f"Uploading ⬆️⬆️⬆️ `{name}`")
+    reply = await m.reply_text(f"**Uploading ⬆️⬆️⬆️**... - `{name}`")
     time.sleep(1)
     start_time = time.time()
     await m.reply_document(ka,caption=cc1)
@@ -200,7 +200,7 @@ async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     await prog.delete (True)
     reply = await m.reply_text(f"**Uploading ⬆️⬆️⬆️ ...** - `{name}`")
     try:
-        if thumb == "no":
+        if thumb == "No":
             thumbnail = f"{filename}.jpg"
         else:
             thumbnail = thumb
